415838e99e6d3d9b0c791f60a71e26ca04b90828
neilhuang <neilhuang@synology.com>
2019-08-24 16:14:07 +0800

    <Resume> Update deploy script to fit at mac environment

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,12 @@
-apt install -y texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
+#! /bin/bash
+
+OS_TYPE="$(uname -s)"
+
+case "${OS_TYPE}" in
+    Darwin*)
+	brew cask install basictex;;
+    Linux)
+	apt install -y texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra;;
+    *)
+	echo "Not Supprt OS Type";;
+esac
