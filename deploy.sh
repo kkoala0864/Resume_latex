45f561dd5ee7496694d8b439d39505d3efef45f9
neilhuang <neilhuang@synology.com>
2025-06-02 11:51:36 +0800

    <Resume> update deploy script for mac

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,9 @@ OS_TYPE="$(uname -s)"
 
 case "${OS_TYPE}" in
     Darwin*)
-	brew cask install basictex;;
+	brew install --cask basictex;;
+	sudo tlmgr update --self
+	sudo tlmgr install collection-fontsrecommended
     Linux)
 	apt install -y texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra;;
     *)
